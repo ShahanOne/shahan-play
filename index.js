6893724bdd3a1bababf3e5163365f512106dbdf2
Syed Shahan Hussain <shahanhussain9105@gmail.com>
2025-02-18 17:33:35 +0530

    Unsplash integration

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const { App } = require('@slack/bolt');
-
+const cron = require('node-cron');
 const port = process.env.PORT || 3000;
 const signingSecret = process.env.SLACK_SIGNING_SECRET;
 const botToken = process.env.SLACK_BOT_TOKEN;
@@ -35,7 +35,7 @@ const app = new App({
           const quote = data[0].q;
           const author = data[0].a;
           await say(
-            `Hi <@${message.user}>, here is a quote for you: "${quote}" - ${author}`
+            `Hi <@${message.user}>, here is a quote for you:\n "${quote}" - ${author}`
           );
         }
       } catch (error) {
@@ -43,6 +43,55 @@ const app = new App({
         await say(
           'Sorry, I could not fetch a quote at the moment. Try again later!'
         );
+      }
+    });
+
+    app.message(async ({ message, say }) => {
+      try {
+        if (
+          message.text.includes(`<@${process.env.BOT_USER_ID}> inspire me`) ||
+          message.text.includes('inspire me')
+        ) {
+          const response = await fetch(
+            `https://api.unsplash.com/photos/random?query=inspiration&client_id=${process.env.UNSPLASH_ACCESS_KEY}`
+          );
+          const data = await response.json();
+
+          if (data.urls) {
+            const imageUrl = data.urls.regular;
+            await say({
+              text: `Here is some inspiration for you, <@${message.user}>!`,
+              attachments: [
+                { image_url: imageUrl, alt_text: 'Inspirational Image' },
+              ],
+            });
+          } else {
+            await say('Sorry, I could not find an inspiring image 😕');
+          }
+        }
+      } catch (error) {
+        console.error('Error fetching image:', error);
+        await say('Oops! Something went wrong while fetching an image.');
+      }
+    });
+
+    // Schedule the daily quote message
+    cron.schedule('0 9 * * *', async () => {
+      try {
+        const response = await fetch('https://zenquotes.io/api/today');
+        const data = await response.json();
+        const quote = data[0].q;
+        const author = data[0].a;
+
+        await app.client.chat.postMessage({
+          token: process.env.SLACK_BOT_TOKEN,
+          channel: 'C08DMHC68N6', //channel ID
+          text: `Good morning! 🌞 Here's a daily quote:\n "${quote}" - ${author}`,
+        });
+
+        console.log('✅ Daily quote sent!');
+      } catch (error) {
+        console.error('Error sending scheduled message:', error);
       }
     });
   } catch (error) {
